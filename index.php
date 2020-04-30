384bdbe991a65b3644d70f9b099a50b8f6db0f7e
Bruno Mota <brunomottta@terra.com.br>
2020-04-30 14:13:35 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página em PHP</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+    <div class="clo-12"><h1>Titulo</h1></div>
+    </div>
+</div>
+    
+</body>
+</html>
